2ad3eafec5746075e4e56d64e28fa2753d4290e3
Ihor Kliushnyk <igorator746@gmail.com>
2026-05-25 17:11:58 +0300

    new project grid animation

--- views/projects/components/ProjectGrid/ProjectsGrid.tsx
+++ views/projects/components/ProjectGrid/ProjectsGrid.tsx
@@ -29,10 +29,10 @@ export const ProjectsGrid = ({
       <motion.div
         key={gridKey}
         className={`${styles.projectsWrapper} ${viewMode === "list" ? styles.listView : ""}`}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.15, ease: "easeInOut" }}
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 6 }}
+        transition={{ duration: 0.2, ease: [0.22, 1, 0.36, 1] }}
       >
         {projects.length > 0 ? (
           <>
